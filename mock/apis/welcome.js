6205d72aa385d69eaa23edbf25b210a4f7f8dc0b
2218675712 <2218675712@qq.com>
2021-01-05 10:19:12 +0800

    添加body-parser插件 获取店铺信息 获取用户信息 添加用餐人数 接口开发完成

--- mock/apis/welcome.js
+++ mock/apis/welcome.js
@@ -1,11 +1,49 @@
 const express = require("express")
 const fileHandle = require("../utils/fileHandle")
+const Unique = require('../utils/Unique')
+const createTime = require("../utils/createTime");
 const router = express.Router()
-router.get("/test", async (req, res, next) => {
-    await fileHandle.add('../files/test.json', {
-        id: 1,
-        name: "张三"
+// 获取店铺信息
+router.get("/getShopMsg", async (req, res, next) => {
+    const {id} = req.query
+    let data = await fileHandle.read('../files/shop')
+    const result = data.find((item) => {
+        return id === id
     })
-    res.send("ok")
+    res.send({
+        code: 200,
+        data: result || {},
+        msg: 'ok'
+    })
+})
+// 获取用户信息
+router.get("/getUserMsg", async (req, res, next) => {
+    const {id} = req.query
+    let data = await fileHandle.read('../files/users')
+    const result = data.find((item) => {
+        return id === id
+    })
+    res.send({
+        code: 200,
+        data: result || {},
+        msg: 'ok'
+    })
+})
+// 添加用餐人数
+router.post("/addPeople", async (req, res, next) => {
+    const {shopId, userId, tableNum, people} = req.body
+    const data = {
+        id: Unique(),
+        shopId: shopId,
+        userId: userId,
+        tableNum: tableNum,
+        people: people,
+        createTime: createTime()
+    }
+    await fileHandle.add('../files/table', data)
+    res.send({
+        code: 200,
+        msg: 'ok'
+    })
 })
 module.exports = router
