640d28a65ba113208a88e44683eab4ff5861098d
2218675712 <2218675712@qq.com>
2021-01-16 21:36:54 +0800

    更新mobx到最新版本

--- src/store/dialog.js
+++ src/store/dialog.js
@@ -1,21 +1,25 @@
-import {action, decorate, observable} from 'mobx'
+import {action, makeObservable, observable} from 'mobx'
 
 class Control {
     msg = "操作成功"
     type = "success"
     show = false
+    // time=1000
     change = action(({msg, type = 'success', show}) => {
         this.msg = msg
         this.type = type
         this.show = show
 
     })
+
+    constructor() {
+        makeObservable(this, {
+            msg: observable,
+            type: observable,
+            show: observable,
+            // time:observable
+        });
+    }
 }
 
-decorate(Control, {
-    msg: observable,
-    type: observable,
-    show: observable,
-    time:observable
-})
 export default new Control()
