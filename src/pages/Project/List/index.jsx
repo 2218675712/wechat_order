ac7e1a4e05128f050d3ef6ef6e35e512a502a364
2218675712 <2218675712@qq.com>
2021-01-20 22:53:00 +0800

    开发头部2 列表渲染

--- src/pages/Project/List/index.jsx
+++ src/pages/Project/List/index.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react'
-import {Route} from 'react-router-dom'
+import {Link, Route} from 'react-router-dom'
 import Styles from './index.module.less'
 import {project} from "../../../services";
 import {getParams} from "../../../utils/tool";
@@ -30,7 +30,48 @@ const List = (props) => {
         }
     }, [])
     return <div id='scroll' className={Styles.List}>
-        <main></main>
+        <main>
+            <nav>
+                <Link to='/project/list'>
+                    <i className='iconfont icon-examineandapprove'></i>
+                    <span>随便点</span>
+                </Link>
+                <Link to='/project/list'>
+                    <i className='iconfont icon-flag_fill'></i>
+                    <span>热销榜</span>
+                </Link>
+                <Link to='/project/list'>
+                    <i className='iconfont icon-document_fill'></i>
+                    <span>点过的菜</span>
+                </Link>
+                <a onClick={() => {
+                }}>
+                    <i className='iconfont icon-remind_fill'></i>
+                    <span>服务铃</span>
+                </a>
+            </nav>
+            {
+                list.map((item, i) => {
+                    let {items = [], index, name} = item
+                    return <section key={index}>
+                        <h3>{name}</h3>
+                        <ul>
+                            {
+                                items.map((jtem, j) => {
+                                    let {name, price, memberPrice, imagePath} = jtem
+                                    return <li key={j}>
+                                        <img src={imagePath} />
+                                        <h4>{name}</h4>
+                                        <b>会员:¥{memberPrice}</b>
+                                        <span>原价:¥{price}</span>
+                                    </li>
+                                })
+                            }
+                        </ul>
+                    </section>
+                })
+            }
+        </main>
     </div>
 }
 const RenderRouter = () => {
