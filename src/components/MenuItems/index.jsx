e07d2ed7c401cbf9be546d2a653ea4ddb4996fbf
2218675712 <2218675712@qq.com>
2021-01-26 22:27:43 +0800

    分类菜单定位,关联渲染

--- src/components/MenuItems/index.jsx
+++ src/components/MenuItems/index.jsx
@@ -1,11 +1,25 @@
 import Styles from './index.module.less'
-import {useState} from "react";
+import {useState,useEffect} from "react";
+import BScroll from "better-scroll";
 
 const MenuItems = (props) => {
-    let {list} = props
+    let {list, findToClass} = props
     const [active, setactive] = useState(false)
+    const findTo = (index) => {
+        setactive(false)
+        findToClass(index)
+    }
+    // 初始化better-scroll
+    useEffect(() => {
+        const scroll = new BScroll("#leftScroll", {
+            click: true
+        })
+        return () => {
+            scroll.destroy()
+        }
+    }, [])
     return <div className={active ? (Styles.active + " " + Styles.MenuItems) : Styles.MenuItems}>
-        <aside className={Styles.asideLeft}>
+        <aside className={Styles.asideLeft} id='leftScroll'>
             <main>
                 <ul>
                     <li>分类</li>
@@ -22,7 +36,7 @@ const MenuItems = (props) => {
                     {
                         list.map((item, i) => {
                             let {items = [], index, name} = item
-                            return <li key={index}>
+                            return <li key={index} onClick={()=>findTo(index)}>
                                 {name}
                             </li>
                         })
--- src/pages/Project/List/index.jsx
+++ src/pages/Project/List/index.jsx
@@ -14,8 +14,6 @@ const List = (props) => {
     const history = props.history
     // 初始化
     useEffect(() => {
-
-
         project.getMenuList(
             {id: getParams("shopId")}
         ).then((data) => {
@@ -35,52 +33,60 @@ const List = (props) => {
             scroll.destroy()
         }
     }, [])
-    return <div id='scroll' className={Styles.List}>
-        {/*<SearchBtn icon='icon-search' cb={() => history.push('/project/search')}/>*/}
-        {/*<BtnLink icon='icon-publishgoods_fill' style={{bottom:'0.4rem',right:'0.2rem'}}>购物车</BtnLink>*/}
-        <MenuItems list={list}>购物车</MenuItems>
-        <main>
-            <nav>
-                <Link to='/project/list'>
-                    <i className='iconfont icon-examineandapprove'></i>
-                    <span>随便点</span>
-                </Link>
-                <Link to='/project/list'>
-                    <i className='iconfont icon-flag_fill'></i>
-                    <span>热销榜</span>
-                </Link>
-                <Link to='/project/list'>
-                    <i className='iconfont icon-document_fill'></i>
-                    <span>点过的菜</span>
-                </Link>
-                <a onClick={() => {
-                }}>
-                    <i className='iconfont icon-remind_fill'></i>
-                    <span>服务铃</span>
-                </a>
-            </nav>
-            {
-                list.map((item, i) => {
-                    let {items = [], index, name} = item
-                    return <section key={index}>
-                        <h3>{name}</h3>
-                        <ul>
-                            {
-                                items.map((jtem, j) => {
-                                    let {name, price, memberPrice, imagePath} = jtem
-                                    return <li key={j}>
-                                        <img src={imagePath}/>
-                                        <h4>{name}</h4>
-                                        <b>会员:¥{memberPrice}</b>
-                                        <span>原价:¥{price}</span>
-                                    </li>
-                                })
-                            }
-                        </ul>
-                    </section>
-                })
-            }
-        </main>
+
+    const findToClass = (index) => {
+        let top = document.getElementById(index).offsetTop
+        console.log(top)
+        scroll.scrollTo(0, -top, 1000)
+    }
+    return <div>
+        <SearchBtn icon='icon-search' cb={() => history.push('/project/search')}/>
+        <BtnLink icon='icon-publishgoods_fill' style={{bottom: '0.4rem', right: '0.2rem'}}>购物车</BtnLink>
+        <MenuItems list={list} findToClass={findToClass}>购物车</MenuItems>
+        <div id='scroll'  className={Styles.List}>
+            <main>
+                <nav>
+                    <Link to='/project/list'>
+                        <i className='iconfont icon-examineandapprove'></i>
+                        <span>随便点</span>
+                    </Link>
+                    <Link to='/project/list'>
+                        <i className='iconfont icon-flag_fill'></i>
+                        <span>热销榜</span>
+                    </Link>
+                    <Link to='/project/list'>
+                        <i className='iconfont icon-document_fill'></i>
+                        <span>点过的菜</span>
+                    </Link>
+                    <a onClick={() => {
+                    }}>
+                        <i className='iconfont icon-remind_fill'></i>
+                        <span>服务铃</span>
+                    </a>
+                </nav>
+                {
+                    list.map((item, i) => {
+                        let {items = [], index, name} = item
+                        return <section key={index} id={index}>
+                            <h3>{name}</h3>
+                            <ul>
+                                {
+                                    items.map((jtem, j) => {
+                                        let {name, price, memberPrice, imagePath} = jtem
+                                        return <li key={j}>
+                                            <img src={imagePath}/>
+                                            <h4>{name}</h4>
+                                            <b>会员:¥{memberPrice}</b>
+                                            <span>原价:¥{price}</span>
+                                        </li>
+                                    })
+                                }
+                            </ul>
+                        </section>
+                    })
+                }
+            </main>
+        </div>
     </div>
 }
 const RenderRouter = () => {
