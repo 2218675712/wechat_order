556e6a7c07831976121f46f95d41e68ed6d50a46
2218675712 <2218675712@qq.com>
2021-02-09 22:38:07 +0800

    详情页结构样式开发

--- src/pages/Project/Detail/index.jsx
+++ src/pages/Project/Detail/index.jsx
@@ -1,21 +1,41 @@
 import Styles from './index.module.less'
 import {Route} from "react-router-dom";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {project} from "../../../services";
 import {getParams} from "../../../utils/tool";
+import GoBack from "../../../components/GoBack";
 
 const Detail = (props) => {
     const {match} = props
+    const [data, setdata] = useState({})
+    const [count, setcount] = useState(1)
     useEffect(() => {
         project.getMenuDetail({
             shopId: getParams('shopId'),
             id: match.params.id
         }).then((data) => {
             console.log(data)
+            setdata(data.data)
         })
     }, [])
     return <div className={Styles.Detail}>
-
+        <GoBack {...props}>返回</GoBack>
+        <img src={data.imagePath} />
+        <section>
+            <h3>{data.name}</h3>
+            <p>¥{data.price}/{data.account}</p>
+        </section>
+        <footer>
+            <aside>
+                <label htmlFor="">数量</label>
+                <p>
+                    <b>-</b>
+                    <span>{count}{data.account}</span>
+                    <b>+</b>
+                </p>
+            </aside>
+            <button className={Styles.shop}>加入购物车</button>
+        </footer>
     </div>
 }
 const RenderRouter = () => {
