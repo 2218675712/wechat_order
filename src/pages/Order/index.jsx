3ddd4e95972529da299404a07f5b584b763cc5b3
2218675712 <2218675712@qq.com>
2021-03-10 10:19:34 +0800

    折叠菜单与提交支付

--- src/pages/Order/index.jsx
+++ src/pages/Order/index.jsx
@@ -7,8 +7,10 @@ import {Link} from "react-router-dom";
 
 
 const Order = (props) => {
+    const {history} = props
     const [data, setdata] = useState({});
     const [type, settype] = useState(false);
+    const [control, setcontrol] = useState({display: 'none'});
     useEffect(() => {
         order.getOrder({
             shopId: getParams('shopId'),
@@ -20,6 +22,22 @@ const Order = (props) => {
         })
     }, [])
     let {menus = []} = data
+    const detailChange = () => {
+        if (type) {
+            setcontrol({display: 'none'})
+        } else {
+            setcontrol({display: 'block'})
+        }
+        settype(!type)
+    }
+    const submit = () => {
+        order.pay({
+            shopId: getParams('shopId'),
+            tableNum: getParams('tableNum')
+        }).then((data) => {
+            history.push('/pc/index' + '?shopId=' + getParams("shopId") + '&userId=' + getParams("userId") + '&tableNum=' + getParams("tableNum"))
+        })
+    }
     return <div className={Styles.Order}>
         <GoBack {...props}>已下单的菜</GoBack>
         <main>
@@ -30,14 +48,14 @@ const Order = (props) => {
                     <section>
                         <p>
                             <b> 合计:{menus.length}项</b>
-                            <button>
+                            <button onClick={detailChange}>
                                 <span>订单详情</span>
                                 <i className={type ? 'iconfont icon-unfold' : 'iconfont icon-packup'}></i>
                             </button>
                         </p>
-                        <ul className={Styles.list}>
-                            {menus.map((item) => {
-                                return <li>
+                        <ul className={Styles.list} style={control}>
+                            {menus.map((item, index) => {
+                                return <li key={index}>
                                     <p>
                                         <span>{item.name}</span>
                                         <i>以下厨</i>
@@ -55,14 +73,14 @@ const Order = (props) => {
                     <ul className={Styles.middle}>
                         <li>
                             <aside>消费金额</aside>
-                            <aside>{data.allPrice}</aside>
+                            <aside>¥{data.allPrice}</aside>
                         </li>
                         <li>
                             <aside>服务费</aside>
-                            <aside>35</aside>
+                            <aside>¥35.00</aside>
                         </li>
                     </ul>
-                    <p>
+                    <p className={Styles.allPriceB}>
                         <span>应付金额</span>
                         <span>¥{parseFloat(data.allPrice) + 35}</span>
                     </p>
@@ -80,7 +98,7 @@ const Order = (props) => {
         <footer>
             <Link
                 to={'/project/list' + '?shopId=' + getParams("shopId") + '&userId=' + getParams("userId") + '&tableNum=' + getParams("tableNum")}>继续点餐</Link>
-            <a href="#">¥{parseFloat(data.allPrice) + 35}立即支付</a>
+            <a href="#" onClick={submit}>¥{parseFloat(data.allPrice) + 35}立即支付</a>
         </footer>
     </div>
 }
