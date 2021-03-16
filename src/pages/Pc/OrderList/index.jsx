500377523a8168d8283438e898ec2525eec1d763
2218675712 <2218675712@qq.com>
2021-03-16 11:44:30 +0800

    获取订单列表

---
+++ src/pages/Pc/OrderList/index.jsx
@@ -0,0 +1,69 @@
+import {useEffect, useState} from "react";
+import {Link, Route} from "react-router-dom";
+import {pc, welcome} from "../../../services";
+import {getParams} from "../../../utils/tool";
+import GoBack from "../../../components/GoBack";
+import Styles from './index.module.less'
+
+
+const OrderList = (props) => {
+    const [list, setlist] = useState([])
+    const [shop, setshop] = useState([])
+    const {history} = props
+    useEffect(() => {
+        pc.getOrder(
+            {
+                userId: getParams("userId")
+            }
+        ).then((data) => {
+            setlist(data.data)
+        })
+        welcome.getShopData(getParams("shopId")).then((data) => {
+            setshop(data.data)
+        })
+    }, [])
+    return <div className={Styles.OrderList}>
+        <GoBack {...props} styles={{top:'0.1rem',left:'0.1rem'}}>返回</GoBack>
+        <main>
+            <div className={Styles.scrollCon}>
+                <h2>我的订单</h2>
+                {
+                    list.map((item, index) => {
+                        return <div className={Styles.item} key={index}>
+                            <section>
+                                <aside>
+                                    <b>{shop.shop_name}</b>
+                                    <span>{item.time}</span>
+                                </aside>
+                                <a href={"tel:" + shop.shopPhone}>
+                                    <i className={'iconfont icon-customerservice_fill'}></i>
+                                </a>
+                            </section>
+                            <ul>
+                                <li>
+                                    订单金额:<span>¥{item.allPrice}</span>
+                                </li>
+                                <li>
+                                    订单状态:<span>{item.isPlay ? '已支付' : '未支付'}</span>
+                                </li>
+                                <li>
+                                    订单编号:<span>{item.id.substr(0, 10) + "..."}</span>
+                                </li>
+                            </ul>
+                            <p>
+                                <Link
+                                    to={"/pc/orderListEvaluate/" + item.id}>我要评价</Link>
+                            </p>
+                        </div>
+                    })
+                }
+            </div>
+
+        </main>
+    </div>
+}
+const RenderRouter = () => {
+    return <Route path='/pc/orderList' component={OrderList}/>
+}
+export default RenderRouter
+
--- src/pages/Pc/index.jsx
+++ src/pages/Pc/index.jsx
@@ -1,5 +1,6 @@
 import Index from './Index/index'
 import Msg from './Msg/index'
+import OrderList from './OrderList/index'
 
 const Project = (props) => {
     return (<div>{props.children}</div>)
@@ -8,6 +9,7 @@ const RenderRouter = () => {
     return <Project>
         <Index/>
         <Msg/>
+        <OrderList/>
     </Project>
 }
 export default RenderRouter
